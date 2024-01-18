1086bba53a9b565da63ee576e05ddb4dba10f3ea
Jude Davis <judedavis92@gmail.com>
2024-01-18 19:46:20 +0000

    feat: json parsing

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+import json
 import requests
 
+from pydantic import BaseModel
 from geopy.distance import geodesic
 
 
 def main():
-    pass
+    get_coords("Northern Road")
+
+
+class OSMResponse(BaseModel):
+    lat: str
+    lon: str
+    display_name: str
 
 def get_coords(address):
     url = f"https://nominatim.openstreetmap.org/search?format=json&q={address}, Slough, UK"
     response = requests.get(url)
-    # results = 
+    json.loads(response.json()[0])
+    OSMResponse.parse_raw()
 
 
 if __name__ == "__main__":
