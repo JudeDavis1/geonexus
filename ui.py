f8e90620ca4c58e0d4c39e2b0da4380c63aeb423
Replit user <>
2024-01-20 00:25:11 +0000

    style(table):

--- ui.py
+++ ui.py
@@ -62,6 +62,24 @@ class GeonexApp(ctk.CTk):
                                           command=self.clear_results)
         self.clear_button.pack(pady=5)
 
+        style = ttk.Style()
+        style.theme_use("default")
+
+        style.configure("Treeview",
+                        background="#2a2d2e",
+                        foreground="white",
+                        rowheight=25,
+                        fieldbackground="#343638",
+                        bordercolor="#343638",
+                        borderwidth=0)
+        style.map('Treeview', background=[('selected', '#22559b')])
+
+        style.configure("Treeview.Heading",
+                        background="#565b5e",
+                        foreground="white",
+                        relief="flat")
+        style.map("Treeview.Heading", background=[('active', '#3484F0')])
+
     def calculate_distances(self):
         self.progress_bar.pack(pady=5)  # Show the progress bar
         self.progress_bar.start()
